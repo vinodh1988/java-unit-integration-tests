89c60505370bd12dccbe72eadb3a0047c63e0760
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-04 10:49:31 +0530

    field-type-commit

---
+++ src/main/java/com/api/entity/Person.java
@@ -0,0 +1,10 @@
+package com.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+public class Person {
+  private Integer sno;
+  private String name;
+  private String city;
+}
